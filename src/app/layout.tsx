6368575c21e637ee42837d793354ac3eec9043fd
YujiAdachi <adachi389@mec-np.com>
2025-01-21 05:50:00 +0900

    chore : update ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,26 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-import { ConfigureAmplifyClientSide } from '@/src/components/ConfigureAmplify';
+import { ConfigureAmplifyClientSide } from "@/src/components/ConfigureAmplify";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Personal RAG Chat',
-  description: 'Personal RAG Chat System',
+	title: "Personal RAG Chat",
+	description: "Personal RAG Chat System",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang='en'>
-      <body className={inter.className}>
-        <ConfigureAmplifyClientSide />
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<ConfigureAmplifyClientSide />
+				{children}
+			</body>
+		</html>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,233 +1,243 @@
-'use client';
+"use client";
 
-import { AuthUser, fetchAuthSession } from '@aws-amplify/auth';
+import { AuthUser, fetchAuthSession } from "@aws-amplify/auth";
 import {
-  Alert,
-  AlertVariations,
-  Button,
-  Flex,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  Tabs,
-  Text,
-  View,
-  withAuthenticator,
-  WithAuthenticatorProps,
-} from '@aws-amplify/ui-react';
-import { AIConversation } from '@aws-amplify/ui-react-ai';
-import { StorageManager } from '@aws-amplify/ui-react-storage';
+	Alert,
+	AlertVariations,
+	Button,
+	Flex,
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableRow,
+	Tabs,
+	Text,
+	View,
+	withAuthenticator,
+	WithAuthenticatorProps,
+} from "@aws-amplify/ui-react";
+import { AIConversation } from "@aws-amplify/ui-react-ai";
+import { StorageManager } from "@aws-amplify/ui-react-storage";
 import {
-  createAmplifyAuthAdapter,
-  createStorageBrowser,
-} from '@aws-amplify/ui-react-storage/browser';
-import '@aws-amplify/ui-react/styles.css';
-import { generateClient } from 'aws-amplify/data';
-import { useCallback, useState } from 'react';
-import Markdown from 'react-markdown';
+	createAmplifyAuthAdapter,
+	createStorageBrowser,
+} from "@aws-amplify/ui-react-storage/browser";
+import "@aws-amplify/ui-react/styles.css";
+import { generateClient } from "aws-amplify/data";
+import { useCallback, useEffect, useState } from "react";
+import Markdown from "react-markdown";
 
-import { type Schema } from '@/amplify/data/resource';
-import { useAIConversation } from '@/src/app/client';
+import { type Schema } from "@/amplify/data/resource";
+import { useAIConversation } from "@/src/app/client";
 
 const client = generateClient<Schema>();
 
-const ChatTab = () => {
-  const [
-    {
-      data: { messages },
-      isLoading,
-      hasError,
-    },
-    handleSendMessage,
-  ] = useAIConversation('chat');
+const ChatTab = ({ user }: { user?: AuthUser }) => {
+	const [issues, setIssues] = useState<Schema["chat"]["type"][]>();
+	const [
+		{
+			data: { messages },
+			isLoading,
+			hasError,
+		},
+		handleSendMessage,
+	] = useAIConversation("chat");
 
-  return (
-    <AIConversation
-      messages={messages}
-      isLoading={isLoading && !hasError}
-      handleSendMessage={handleSendMessage}
-    />
-  );
+	const handleFetchIssues = useCallback(async () => {
+		if (!user?.username) return;
+
+		const result = await client.conversations.chat.list();
+		if (!result.data) return;
+		setIssues(result.data);
+	}, [user]);
+
+	useEffect(() => {
+		handleFetchIssues();
+	}, [handleFetchIssues]);
+
+	return (
+		<Flex direction="row" rowGap="l">
+			<AIConversation
+				messages={messages}
+				isLoading={isLoading && !hasError}
+				handleSendMessage={handleSendMessage}
+			/>
+		</Flex>
+	);
 };
 
 const StorageTab = () => {
-  const { StorageBrowser } = createStorageBrowser({
-    config: createAmplifyAuthAdapter(),
-  });
-  const [creatingState, setCreatingState] = useState<AlertVariations>();
+	const { StorageBrowser } = createStorageBrowser({
+		config: createAmplifyAuthAdapter(),
+	});
+	const [creatingState, setCreatingState] = useState<AlertVariations>();
 
-  const handleMakeKnowledge = useCallback(async ({ key }: { key?: string }) => {
-    if (!key) {
-      return;
-    }
+	const handleMakeKnowledge = useCallback(async ({ key }: { key?: string }) => {
+		if (!key) {
+			return;
+		}
 
-    setCreatingState('info');
-    const session = await fetchAuthSession();
-    const result = await client.queries.createKnowledge({
-      identityID: session.identityId,
-      accessLevel: 'protected',
-      uploadedKey: key,
-    });
+		setCreatingState("info");
+		const session = await fetchAuthSession();
+		const result = await client.queries.createKnowledge({
+			identityID: session.identityId,
+			accessLevel: "protected",
+			uploadedKey: key,
+		});
 
-    if (!result.data) {
-      setCreatingState('error');
-      return;
-    }
+		if (!result.data) {
+			setCreatingState("error");
+			return;
+		}
 
-    setCreatingState('success');
-  }, []);
+		setCreatingState("success");
+	}, []);
 
-  const getAlertMessage = () => {
-    switch (creatingState) {
-      case 'info':
-        return '情報を整理中です...';
-      case 'error':
-        return 'エラーが発生しました';
-      case 'success':
-        return '情報の整理が完了しました';
-      default:
-        return '';
-    }
-  };
+	const AlertMessage: { [key in AlertVariations]?: string } = {
+		info: "情報を整理中です...",
+		error: "エラーが発生しました",
+		success: "情報の整理が完了しました",
+	};
 
-  return (
-    <Flex direction='column' rowGap='l'>
-      <Alert variation={creatingState}>{getAlertMessage()}</Alert>
-      <StorageManager
-        acceptedFileTypes={['image/*', 'application/pdf']}
-        accessLevel='protected'
-        onUploadSuccess={handleMakeKnowledge}
-        maxFileCount={1}
-      />
+	return (
+		<Flex direction="column" rowGap="l">
+			<Alert variation={creatingState}>
+				{creatingState && AlertMessage[creatingState]}
+			</Alert>
+			<StorageManager
+				acceptedFileTypes={["image/*", "application/pdf"]}
+				accessLevel="private"
+				onUploadSuccess={handleMakeKnowledge}
+				maxFileCount={1}
+			/>
 
-      <StorageBrowser />
-    </Flex>
-  );
+			<StorageBrowser />
+		</Flex>
+	);
 };
 
 const KnowledgeTab = ({ user }: { user?: AuthUser }) => {
-  const [selectedItems, setSelectedItems] = useState<
-    Schema['UserKnowledgeContent']['type'][]
-  >([]);
-  const [knowledge, setKnowledge] =
-    useState<Schema['UserKnowledge']['type'][]>();
+	const [selectedItems, setSelectedItems] = useState<
+		Schema["UserKnowledgeContent"]["type"][]
+	>([]);
+	const [knowledge, setKnowledge] =
+		useState<Schema["UserKnowledge"]["type"][]>();
+	const isSelected = selectedItems.length > 0;
 
-  const isSelected = selectedItems.length > 0;
-  const handleFetchKnowledge = useCallback(async () => {
-    if (!user?.username) return;
+	const handleFetchKnowledge = useCallback(async () => {
+		if (!user?.username) return;
 
-    const result =
-      await client.models.UserKnowledge.listUserKnowledgeByUsername({
-        username: user.username,
-      });
-    if (!result.data) return;
+		const result =
+			await client.models.UserKnowledge.listUserKnowledgeByUsername({
+				username: user.username,
+			});
+		if (!result.data) return;
 
-    setKnowledge(result.data);
-  }, [user]);
+		setKnowledge(result.data);
+	}, [user]);
 
-  return (
-    <Flex direction='column' rowGap='l'>
-      <Button
-        size='small'
-        variation='primary'
-        onClick={() =>
-          isSelected ? setSelectedItems([]) : handleFetchKnowledge()
-        }
-      >
-        {isSelected ? 'Back' : 'Fetch Knowledge'}
-      </Button>
+	return (
+		<Flex direction="column" rowGap="l">
+			<Button
+				size="small"
+				variation="primary"
+				onClick={() =>
+					isSelected ? setSelectedItems([]) : handleFetchKnowledge()
+				}
+			>
+				{isSelected ? "Back" : "Fetch Knowledge"}
+			</Button>
 
-      {isSelected ? (
-        <Flex direction='column' rowGap='l'>
-          {selectedItems.map((content) => (
-            <Flex key={content.id} direction='column' rowGap='l'>
-              <Markdown>{content.markdown}</Markdown>
-            </Flex>
-          ))}
-        </Flex>
-      ) : (
-        <Table highlightOnHover={true} variation='striped'>
-          <TableHead>
-            <TableRow>
-              <TableCell as='th'>Title</TableCell>
-              <TableCell as='th'>Abstract</TableCell>
-              <TableCell as='th'>CreatedAt</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {knowledge?.map((item) => (
-              <TableRow
-                key={item.id}
-                onClick={async () => {
-                  const findItem = knowledge.find((k) => k.id === item.id);
-                  const selected = await findItem?.contents();
-                  if (selected?.data) {
-                    // page番号で並び替え
-                    const items = selected.data.sort((a, b) => {
-                      return Number(a.page) - Number(b.page);
-                    });
-                    setSelectedItems(items);
-                  }
-                }}
-              >
-                <TableCell>{item.key}</TableCell>
-                <TableCell>
-                  <Text fontSize='12px'>{item.abstract}</Text>
-                </TableCell>
-                <TableCell>{item.createdAt}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      )}
-    </Flex>
-  );
+			{isSelected ? (
+				<Flex direction="column" rowGap="l">
+					{selectedItems.map((content) => (
+						<Flex key={content.id} direction="column" rowGap="l">
+							<Markdown>{content.markdown}</Markdown>
+						</Flex>
+					))}
+				</Flex>
+			) : (
+				<Table highlightOnHover={true} variation="striped">
+					<TableHead>
+						<TableRow>
+							<TableCell as="th">Title</TableCell>
+							<TableCell as="th">Abstract</TableCell>
+							<TableCell as="th">CreatedAt</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{knowledge?.map((item) => (
+							<TableRow
+								key={item.id}
+								onClick={async () => {
+									const findItem = knowledge.find((k) => k.id === item.id);
+									const selected = await findItem?.contents();
+									if (selected?.data) {
+										// page番号で並び替え
+										const items = selected.data.sort((a, b) => {
+											return Number(a.page) - Number(b.page);
+										});
+										setSelectedItems(items);
+									}
+								}}
+							>
+								<TableCell>{item.key}</TableCell>
+								<TableCell>
+									<Text fontSize="12px">{item.abstract}</Text>
+								</TableCell>
+								<TableCell>{item.createdAt}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			)}
+		</Flex>
+	);
 };
 
 const App = ({ signOut, user }: WithAuthenticatorProps) => {
-  return (
-    <Flex justifyContent='center' alignItems='center'>
-      <Flex
-        direction='column'
-        rowGap='l'
-        flex={1}
-        width='100%'
-        maxWidth='1280px'
-      >
-        <Flex direction='row' rowGap='l'>
-          <View flex={1} textAlign='center'>
-            <h1>Personal RAG Chat</h1>
-          </View>
-          <Flex justifyContent='center' alignItems='center'>
-            <Button size='small' variation='menu' onClick={signOut}>
-              Sign out
-            </Button>
-          </Flex>
-        </Flex>
+	return (
+		<Flex justifyContent="center" alignItems="center">
+			<Flex
+				direction="column"
+				rowGap="l"
+				flex={1}
+				width="100%"
+				maxWidth="1280px"
+			>
+				<Flex direction="row" rowGap="l">
+					<View flex={1} textAlign="center">
+						<h1>Personal RAG Chat</h1>
+					</View>
+					<Flex justifyContent="center" alignItems="center">
+						<Button size="small" variation="menu" onClick={signOut}>
+							Sign out
+						</Button>
+					</Flex>
+				</Flex>
 
-        <Flex direction='row' rowGap='xxl'>
-          <Tabs.Container defaultValue='1' flex={1}>
-            <Tabs.List spacing='equal'>
-              <Tabs.Item value='1'>Chat</Tabs.Item>
-              <Tabs.Item value='2'>Storage</Tabs.Item>
-              <Tabs.Item value='3'>Knowledge</Tabs.Item>
-            </Tabs.List>
-            <Tabs.Panel value='1'>
-              <ChatTab />
-            </Tabs.Panel>
-            <Tabs.Panel value='2'>
-              <StorageTab />
-            </Tabs.Panel>
-            <Tabs.Panel value='3'>
-              <KnowledgeTab user={user} />
-            </Tabs.Panel>
-          </Tabs.Container>
-        </Flex>
-      </Flex>
-    </Flex>
-  );
+				<Flex direction="row" rowGap="xxl">
+					<Tabs.Container defaultValue="1" flex={1}>
+						<Tabs.List spacing="equal">
+							<Tabs.Item value="1">Chat</Tabs.Item>
+							<Tabs.Item value="2">Storage</Tabs.Item>
+							<Tabs.Item value="3">Knowledge</Tabs.Item>
+						</Tabs.List>
+						<Tabs.Panel value="1">
+							<ChatTab user={user} />
+						</Tabs.Panel>
+						<Tabs.Panel value="2">
+							<StorageTab />
+						</Tabs.Panel>
+						<Tabs.Panel value="3">
+							<KnowledgeTab user={user} />
+						</Tabs.Panel>
+					</Tabs.Container>
+				</Flex>
+			</Flex>
+		</Flex>
+	);
 };
 
 export default withAuthenticator(App);
